ca2f972e31f31758dfe4867c26fbd527a12a7f1a
brnydr <brianwilliamyoder@gmail.com>
2023-08-03 14:42:41 -0700

    fix apiservice

--- src/ApiService.js
+++ src/ApiService.js
@@ -1,13 +1,13 @@
 export default class MovieApiService {
   static async getMovie(movie) {
     try {
-      fetch(`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${movie}`);
-      let response = response.json();
+      const response =  await fetch(`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${movie}`);
+      const jsonResponse = await response.json();
       if (!response.ok) {
-        const errorMessage = "Error";
+        const errorMessage = `${response.status} ${response.statusText} ${jsonResponse.message}`;
         throw new Error (errorMessage);
       }
-      return response;
+      return jsonResponse;
     }
     catch(error) {
       return error;
