012211f4870bad9a4d96f7215965ce21b16c7852
brnydr <brianwilliamyoder@gmail.com>
2023-08-03 13:14:23 -0700

    create getMovie function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import MovieApiService from './ApiService';
 
+async function getMovie(movie) {
+  const response = MovieApiService.getMovie(movie);
+  if (response.search) {
+    printMovie(response, movie);
+  } else {
+    printError(response, movie);
+  }
+}
 //this is where UI logic lives. 
+
