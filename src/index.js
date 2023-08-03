9c212216c4711d15a944218c1a0eb228a3642e0a
brnydr <brianwilliamyoder@gmail.com>
2023-08-03 14:43:55 -0700

    refactor printMovie

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import MovieApiService from './ApiService';
 
 async function getMovie(movie) {
   const response = await MovieApiService.getMovie(movie);
-  if (response.search) {
+  if (response.Search) {
     printMovie(response, movie);
   } else {
     printError(response, movie);
@@ -13,10 +13,25 @@ async function getMovie(movie) {
 }
 
 function printMovie(response) {
-  let movieOutput = document.querySelector("#movieOutput");
-  let movieList = response.Search;
-  movieOutput.innerText = movieList;
+  // let movieOutput= document.getElementById("movieOutput");
+  let movieList = document.querySelector("#movieList");
+  let movieTitle = [];
+  response.Search.forEach(movie => {
+    movieTitle.push(movie.Title);
+  });
+  movieTitle.forEach(title => {
+    let li = document.createElement("li");
+    li.innerText = title;
+    movieList.appendChild(li);
+  });
+ let button =  document.createElement("button");
+ button.innerText = "Clear";
+ button.setAttribute("id", "clearButton");
+ movieList.append(button);
+ button.addEventListener("click", reloadPage);
+ 
 }
+
 
 function printError(response) {
   document.querySelector("#movieOutput").innerText = `There was an error handling your request: ${response.message}`;
@@ -29,6 +44,11 @@ function handleFormSubmission(e) {
   getMovie(movieTitle);
 }
 
-window.addEventListener("load", () => { 
-  document.querySelector("#movieForm").addEventListener("submit", handleFormSubmission);
+function reloadPage() {
+  location.reload();
+}
+
+window.addEventListener("load", function(){ 
+  document.querySelector("form").addEventListener("submit", handleFormSubmission);
 });
+
