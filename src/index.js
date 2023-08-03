047a4cd58348d8605ac8a42a0243b9e195a49048
brnydr <brianwilliamyoder@gmail.com>
2023-08-03 13:31:52 -0700

    create ui logic

--- src/index.js
+++ src/index.js
@@ -4,12 +4,31 @@ import './css/styles.css';
 import MovieApiService from './ApiService';
 
 async function getMovie(movie) {
-  const response = MovieApiService.getMovie(movie);
+  const response = await MovieApiService.getMovie(movie);
   if (response.search) {
     printMovie(response, movie);
   } else {
     printError(response, movie);
   }
 }
-//this is where UI logic lives. 
 
+function printMovie(response) {
+  let movieOutput = document.querySelector("#movieOutput");
+  let movieList = response.Search;
+  movieOutput.innerText = movieList
+}
+
+function printError(response) {
+  document.querySelector("#movieOutput").innerText = `There was an error handling your request: ${response.message}`;
+}
+
+
+function handleFormSubmission(e) {
+  e.preventDefault();
+  let movieTitle = document.querySelector("#movieTitle").value;
+  getMovie(movieTitle);
+}
+
+window.addEventListener("load", () => { 
+  document.querySelector("#movieForm").addEventListener("submit", handleFormSubmission);
+});
